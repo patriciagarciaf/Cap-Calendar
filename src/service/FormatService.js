8000d39f413226a3f8b571315f8141f6a5ad21ba
DamasoGB <dimnas12@gmail.com>
2021-09-06 23:47:47 +0200

    FormatService devuelve hora,min,seg de una fecha por param.

--- src/service/FormatService.js
+++ src/service/FormatService.js
@@ -1,5 +1,14 @@
 class FormatService{
-    static getTime(data){
-        return " hora;min;seg";
+    static getTime(date){
+        const timeOptions = {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+        };
+          
+        const formatter = new Intl.DateTimeFormat('es-ES', {
+            timeZone: 'Europe/Madrid', ...timeOptions
+        });
+        return formatter.format(date);
     }
 }
