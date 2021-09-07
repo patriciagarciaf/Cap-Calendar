b7fa7d6d698d545f3f39a2ab4e59c8e24748bdc9
Irene Guillen <irene.guillen-zapata@...com>
2021-09-07 10:59:12 +0200

    update sateservice

--- src/service/DateService.js
+++ src/service/DateService.js
@@ -6,4 +6,20 @@ class DateService{
     addMonth(date, diff){
 
     }
+
+    timer();
+daysWeek();
+
+timer(date){
+
+    return new Intl.DateTimeFormat('en-GB', {dateStyle: 'full'}).format(date);
+}
+
+daysWeek(){
+
+    for (let i = 0; i < DAYSOFWEEK.length(); i++)
+		{
+			return DAYSOFWEEK[i] + "  ";
+		}
+}
 }
