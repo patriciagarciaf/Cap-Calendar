9d4fccdeb806fdc28ed1151b1ca0c7d0c91caeea
DamasoGB <dimnas12@gmail.com>
2021-09-08 10:57:35 +0200

    fixes varios

--- src/components/basedatecomponent.js
+++ src/components/basedatecomponent.js
@@ -1,23 +1,5 @@
 export class BaseDateComponent extends HTMLElement{
-    constructor() {
-        super();
-        this._date = new Date();
-        this._shadow = this.attachShadow({mode: "open"})
-    }
-    _create(){
-        let div= document.createElement('div');
-        let text = document.createTextNode(this._formatDate());
-        div.appendChild(text);
-        this._shadow.appendChild(div);
-        return text;
-    }
-    _update(node,date){
-        this._date = date;
-        node.data = this._formatDate();
-    }
-    disconnectedCallback(){
-
-    }
+    
     _formatDate(){
         throw "Method not implemented"
     }
--- src/components/clock/clock.js
+++ src/components/clock/clock.js
@@ -1,12 +1,32 @@
 import { FormatService } from '../../service/FormatService.js'
 import pubSub from '../../service/PubSub.js'
 import { CHANELS } from '../../service/Config.js'
-import { BaseDateComponent } from '../basedatecomponent.js';
 
-export class Clock extends BaseDateComponent{
+export class Clock extends HTMLElement{
+    constructor() {
+        super();
+        this._disposables=[];
+        this._date = new Date();
+        this._shadow = this.attachShadow({mode: "open"})
+    }
     connectedCallback(){
-        const texto = super._create();
-        const disposable = pubSub.on(CHANELS.CHANGEDATE, (date) => super._update(texto,date));
+        const text = this._create();
+        const disposable = pubSub.on(CHANELS.CHANGEDATE, (date) => this._update(text,date));
+        this._disposables.push(disposable);
+    }
+    disconnectedCallback(){
+        this._disposables=[];
+    }
+    _create(){
+        let div= document.createElement('div');
+        let text = document.createTextNode(this._formatDate());
+        div.appendChild(text);
+        this._shadow.appendChild(div);
+        return text;
+    }
+    _update(node,date){
+        this._date = date;
+        node.data = this._formatDate();
     }
     _formatDate(){
         return FormatService.getTime(this._date)
