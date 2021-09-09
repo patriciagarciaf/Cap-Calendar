c14714595358942abc41a7777e8056bba285767c
Irene Guillen <irene.guillen-zapata@...com>
2021-09-09 08:42:44 +0200

    correct dateservice

--- src/service/DateService.js
+++ src/service/DateService.js
@@ -1,29 +1,19 @@
-
-class DateService {
+export class DateService {
 
     constructor(){
         this._button = document.getElementsByName("");
     }
-
-
-export class DateService {
-
     static getMonthDays(date) {
         return [];
     }
-
-
     static addMonth(date, diff) {
 
         date.setMonth(date.getMonth() + diff)
         returnDateService.getDayOfMonth(date);
     }
-
-
     static isToday(date, today) {
         return date.getDate() === today.getDate() &&
             date.getMonth() === today.getMonth() &&
             date.getFullYear() == today.getFullYear()
     }
-
 }
