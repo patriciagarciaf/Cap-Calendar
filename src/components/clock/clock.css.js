af24402925506135d24e70b259fe3354d85f1a03
DamasoGB <dimnas12@gmail.com>
2021-09-09 12:19:34 +0200

    css reloj

--- src/components/clock/clock.css.js
+++ src/components/clock/clock.css.js
@@ -1,7 +1,10 @@
 const sheet = new CSSStyleSheet();
 sheet.replace(`
 :host {
+    background-color: var(--backgroundcolor);
+    color: var(--textcolor);
     font-size: 3em;
+    font-family: var(--font)
 }
 `);
 export default sheet;
