bfe86be99389a5e034e059075f8ed485b5591b08
patriciagarciaf <88269955+patriciagarciaf@users.noreply.github.com>
2021-09-09 12:02:55 +0200

    basedatecomponent added

--- src/components/basedatecomponent.js
+++ src/components/basedatecomponent.js
@@ -1,6 +1,60 @@
+import PubSub from '../service/PubSub.js'
+import {CHANNELS} from '../service/Config.js'
+const METHOD_NOT_IMPLEMENTED='Method not implemented'
+
 export class BaseDateComponent extends HTMLElement{
-    
+
+    constructor(){
+        super();
+        this._create();
+        this.date=new Date();
+        this._oldDate=new Date();
+        this._disposables=[];
+    }
+    set date(value){
+        this._oldDate=this.date;
+        if(this._changeDate(value)){
+            this._date=value;
+            this._update();
+        }
+    }
+    get date(){
+        return this._date;
+    }
+    connectedCallback(){
+        this._suscribe();
+    }
+    _suscribe(channel=CHANNELS.CHANGEDATE){
+        const dispose=PubSub.on(channel,(date)=>{
+            this.date=date;
+        });
+        this._disposables.push(dispose);
+    }
+    disconnectedCallback(){
+        this._disposables.forEach(dispose=>{
+            dispose && dispose();
+        })
+        this._disposables=[];
+    }
+    _getStyle(){
+        throw METHOD_NOT_IMPLEMENTED;
+    }
+    _changeDate(value){
+        throw METHOD_NOT_IMPLEMENTED;
+    }
+    _create(){
+        this._text=document.createTextNode("");
+        this._shadow=this.attachShadow({ mode: "open" });
+        const style=this._getStyle();
+        if (style){
+            this._shadow.appendChild(style);
+        }
+        this._shadow.appendChild(this._text);
+    }
+    _update(){
+        this._text.data = this._formatDate();
+    }
     _formatDate(){
-        throw "Method not implemented"
+        throw METHOD_NOT_IMPLEMENTED;
     }
 }
--- src/components/clock/clock.js
+++ src/components/clock/clock.js
@@ -2,42 +2,18 @@ import { FormatService } from '../../service/FormatService.js'
 import pubSub from '../../service/PubSub.js'
 import { CHANNELS } from '../../service/Config.js'
 import css from './clock.css.js'
+import { BaseDateComponent } from '../basedatecomponent.js'
 
-export class Clock extends HTMLElement{
-    constructor() {
-        super();
-        this._disposables=[];
-        this._date = new Date();
-        this._shadow = this.attachShadow({mode: "open"})
+export class Clock extends BaseDateComponent{
 
-    }
-    connectedCallback(){
-        const text = this._create();
-        const disposable = pubSub.on(CHANNELS.CHANGEDATE, (date) => this._update(text,date));
-        this._disposables.push(disposable);
-    }
-    disconnectedCallback(){
-        this._shadow.textContent = "";
-        this._disposables=[];
-    }
-    _create(){
-        let text = document.createTextNode(this._formatDate());
-        const style = this._getStyle();
-        if (style) {
-            this._shadow.appendChild(style);
-        }
-        this._shadow.appendChild(text);
-        return text;
-    }
-    _update(node,date){
-        this._date = date;
-        node.data = this._formatDate();
-    }
     _formatDate(){
         return FormatService.getTime(this._date)
     }
     _getStyle(){
         this._shadow.adoptedStyleSheets = [css];
+    }
+    _changeDate(value){
+        return true;
     }
 }
 customElements.define("cap-clock", Clock);
