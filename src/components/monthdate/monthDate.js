7e190460076d6e89b3d41561020787dce2c67997
patriciagarciaf <88269955+patriciagarciaf@users.noreply.github.com>
2021-09-12 18:03:17 +0200

    update

--- src/components/monthdate/monthDate.js
+++ src/components/monthdate/monthDate.js
@@ -4,27 +4,27 @@ import { BaseDateComponent } from "../baseDateComponent/basedatecomponent.js";
 import css from "./monthDate.css.js"
 
 export class MonthDate extends BaseDateComponent{
-    constructor() {
-        super();
-        this._shadow.adoptedStyleSheets = [...this._shadow.adoptedStyleSheets, css];
-    }
-    _formatDate() {
-        return FormatService.getMonth(this._date)
-    }
-    connectedCallback() {
-       const texto = super._create()
-       const disposableDate = pubSub.on(CHANNELS.CHANGEDATE, (date) => {
-            if (!DateService.isThisMonth(date, new Date())) {
-                super._update(texto,date)
-            }
-        })
-        const disposableMonth = pubSub.on(CHANNELS.CHANGEMONTH, (diff) => {
-            let newDate = this._date;
-            newDate.setMonth(this._date.getMonth() + diff);
-            super._update(texto,newDate)
-        })
-        this._disposables.push(disposableDate, disposableMonth);
-    }
+    // constructor() {
+    //     super();
+    //     this._shadow.adoptedStyleSheets = [...this._shadow.adoptedStyleSheets, css];
+    // }
+    // _formatDate() {
+    //     return FormatService.getMonth(this._date)
+    // }
+    // connectedCallback() {
+    //    const texto = super._create()
+    //    const disposableDate = pubSub.on(CHANNELS.CHANGEDATE, (date) => {
+    //         if (!DateService.isThisMonth(date, new Date())) {
+    //             super._update(texto,date)
+    //         }
+    //     })
+    //     const disposableMonth = pubSub.on(CHANNELS.CHANGEMONTH, (diff) => {
+    //         let newDate = this._date;
+    //         newDate.setMonth(this._date.getMonth() + diff);
+    //         super._update(texto,newDate)
+    //     })
+    //     this._disposables.push(disposableDate, disposableMonth);
+    // }
     _setStyle() {
         this._shadow.adoptedStyleSheets = [...this._shadow.adoptedStyleSheets, css];
     }
