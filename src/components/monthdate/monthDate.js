cac7887dcce7fbf1680790ecef92204547e7ae90
DamasoGB <dimnas12@gmail.com>
2021-09-12 23:08:37 +0200

    monthDate va con los botones

--- src/components/monthdate/monthDate.js
+++ src/components/monthdate/monthDate.js
@@ -1,7 +1,9 @@
 import { FormatService } from "../../service/FormatService.js";
 import { DateService } from "../../service/DateService.js";
 import { BaseDateComponent } from "../baseDateComponent/basedatecomponent.js";
-import css from "./monthDate.css.js"
+import pubSub from "../../service/PubSub.js";
+import css from "./monthDate.css.js";
+import { CHANNELS } from "../../service/Config.js";
 
 export class MonthDate extends BaseDateComponent{
     _formatDate() {
@@ -16,20 +18,20 @@ export class MonthDate extends BaseDateComponent{
         this._shadow.adoptedStyleSheets = [...this._shadow.adoptedStyleSheets, css];
     }
 
-    // connectedCallback() {
-    //     const texto = super._create();
-    //     const disposableDate = pubSub.on(CHANNELS.CHANGEDATE, (date) => {
-    //          if (!DateService.isThisMonth(date, new Date())) {
-    //              super._update(texto,date)
-    //          }
-    //      })
-    //      const disposableMonth = pubSub.on(CHANNELS.CHANGEMONTH, (diff) => {
-    //          let newDate = this._date;
-    //          newDate.setMonth(this._date.getMonth() + diff);
-    //          super._update(texto,newDate)
-    //      })
-    //      this._disposables.push(disposableDate, disposableMonth);
-    //  }
+    connectedCallback() {
+        const texto = super._create
+        const disposableDate = pubSub.on(CHANNELS.CHANGEDATE, (date) => {
+             if (!DateService.isThisMonth(date, new Date())) {
+                 super._update(texto,date)
+             }
+         })
+         const disposableMonth = pubSub.on(CHANNELS.CHANGEMONTH, (diff) => {
+             let newDate = this._date;
+             newDate.setMonth(this._date.getMonth() + diff);
+             super._update(texto,newDate)
+         })
+         this._disposables.push(disposableDate, disposableMonth);
+     }
 }
 
 customElements.define('cap-month-date', MonthDate);
