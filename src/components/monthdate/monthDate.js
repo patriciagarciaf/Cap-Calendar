621067ccf7985aeb124ae7ab3244aa60f6d93fe7
DamasoGB <dimnas12@gmail.com>
2021-09-12 12:07:22 +0200

    monthdate ahora sabe cuando cambiar

--- src/components/monthdate/monthDate.js
+++ src/components/monthdate/monthDate.js
@@ -4,16 +4,26 @@ import { BaseDateComponent } from "../baseDateComponent/basedatecomponent.js";
 import sheet from "./monthDate.css.js"
 
 export class MonthDate extends BaseDateComponent{
+    constructor() {
+        super();
+        this._shadow.adoptedStyleSheets = [...this._shadow.adoptedStyleSheets, css];
+    }
     _formatDate() {
-        return FormatService.getMonth(this.date);
+        return FormatService.getMonth(this._date)
     }
-
-    _changeDate(value) {
-        return !this.date || !DateService.isThisMonth(value, this._oldDate);
-    }
-
-    _setStyle() {
-        this._shadow.adoptedStyleSheets = [...this._shadow.adoptedStyleSheets, sheet];
+    connectedCallback() {
+       const texto = super._create()
+       const disposableDate = pubSub.on(CHANNELS.CHANGEDATE, (date) => {
+            if (!DateService.isThisMonth(date, new Date())) {
+                super._update(texto,date)
+            }
+        })
+        const disposableMonth = pubSub.on(CHANNELS.CHANGEMONTH, (diff) => {
+            let newDate = this._date;
+            newDate.setMonth(this._date.getMonth() + diff);
+            super._update(texto,newDate)
+        })
+        this._disposables.push(disposableDate, disposableMonth);
     }
 }
 
